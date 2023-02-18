3d236d54ab4abd32a855435a8bebc70cbcee230d
JulieSN <julietsvuura@gmail.com>
2023-02-17 19:08:22 -0800

    goo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = '0'; digit1 < '10'; digit1++)
+	for (digit1 = '0'; digit1 < 10; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < '10'; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 		if (digit2 != digit1)
 		{
 		putchar((digit1));
 		putchar((digit2));
 
-			if (digit1 == 8 && digit2 == 10)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 
 			putchar(',');
