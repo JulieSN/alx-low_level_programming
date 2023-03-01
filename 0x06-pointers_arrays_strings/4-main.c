3792cbe826a99d6404994c21c1735f5e692c024f
JulieSN <julietsvuura@gmail.com>
2023-03-01 12:49:10 -0800

    hekelm

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -3,6 +3,7 @@
 
 /**
  * main - check the code
+ * print_array - print array
  * @a: an array of integers
  * @n: the number of elements to swap
  *
@@ -10,17 +11,17 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
 }
